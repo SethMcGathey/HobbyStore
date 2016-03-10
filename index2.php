7fa40b457a20cfb99db70247bb3aee7c15691d14
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-10 13:53:19 -0600

    simplified index2.php for testing

--- index2.php
+++ index2.php
@@ -1,75 +1,15 @@
-<?php require_once 'sessionStart.php'; ?>
-<?php require_once 'databaseClasses/category.php'; ?>
-<?php require_once 'subcategory.php'; ?>
-<!DOCTYPE html>
-<html lang="en">
-	<?php require_once 'header.php' ?>
+<?php 
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
 
-	<body>
+require_once 'sessionStart.php';
+require_once 'databaseClasses/category.php';
+require_once 'databaseClasses/subcategory.php';
 
-		<?php require_once 'navigation.php' ?> 
-		<div class="container-fluid" id="Not_Ajax_Output">
-			<h1 class="centerText">Home Page</h1>
-			<div class="row">
+$category = new categoryDataAccess();
+$category->readData(1);
 
-              <?php
-              error_reporting(E_ALL);
-				ini_set('display_errors', 'on');
-               $category = new categoryDataAccess();
-               $category->readData(1);
-
-               $sql = 'SELECT id, name FROM category ORDER BY id';
-               $num = 0;
-               foreach ($pdo->query($sql) as $row) {
-	                echo '<a href="#">
-	                		<div class="col-lg-4 myCategories categoryBackgroundColor' . $num . '" id="' . $row['id']. '">
-	                			<img src="img/rrwggame.jpg" width="100px" class="categoryImage"/><p class="centerText">' . $row['name'] . '</p>
-	                		</div>
-	                	  </a>';
-	               	if($num < 1)
-	               	{
-	                	$num++;
-	                }else
-	                {
-						$num = 0;
-	                }
-               }
-
-
-              ?>
-          	</div>
-
-			<div id="inner_ajax_Output">
-				<?php
-				    $sql = 'SELECT id,name,category_id FROM subcategory WHERE category_id = 1';
-				    $num = 0;
-					foreach ($pdo->query($sql) as $row) {
-						//echo 'hello';
-				    	echo '<a href="products.php?id=' . $row['id']. '"><div class="col-lg-12 subcategoryColor' . $num . '" id="' . $row['id']. '"><p class="leftRight' . $num . '"">' . $row['name'] . '</p></div></a>';
-
-				    	if($num < 1){
-				    		$num++;
-				    	}else
-				    	{
-				    		$num = 0;
-				    	}
-					}
-				?>
-			</div>
-		</div>
-
-		<div class="container-fluid" id="ajax_Output">
-		</div>
-
-
-<?php require_once 'footer.php' ?>
-
-
-	</body>
-
-	
-</html>
-
+              
 
 
 
