fe3cd96f0938ec0b0a449f8d7ae63cc8af7184aa
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-10 14:02:50 -0600

    commented out extra distrubtive code

--- accessDatabaseClass.php
+++ accessDatabaseClass.php
@@ -4,14 +4,14 @@
     $pdo = Database::connect();
 
 abstract class accessDatabase{
-	abstract function insertData($tableColumnsArray);
+/*	abstract function insertData($tableColumnsArray);
 	abstract function updateData($tableColumnsArray);
 
 	public function selectData($tableName){
 		$sql = 'SELECT * FROM $tableName';
 	};
 
-
+*/
 	public function doSql($inputValues, $sqlVar){
 		try{
 			$pdo = Database::connect();
