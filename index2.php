e16e06b382476b992f8f5a56f78bdec28dd7c5ee
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-10 15:50:53 -0600

    trying single foreach

--- index2.php
+++ index2.php
@@ -12,9 +12,9 @@ require_once 'databaseClasses/subcategoryClass.php';
 $category = new categoryDataAccess();
 $category->readData(1);
 print_r($category->readData(1));
-foreach ($category->readData(1) as $row) {
-	foreach($row as $innerRow)
+//foreach ($category->readData(1) as $row) {
+	foreach($category->readData(1)[1] as $innerRow)
 	{
 		echo $name = $innerRow['name'];
 	}
-}
+//}
