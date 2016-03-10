a7edc12a2f109d8f3ebe7c6080107134069035d0
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-10 13:54:34 -0600

    added class to the end of class references

--- index2.php
+++ index2.php
@@ -3,8 +3,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
 require_once 'sessionStart.php';
-require_once 'databaseClasses/category.php';
-require_once 'databaseClasses/subcategory.php';
+require_once 'databaseClasses/categoryClass.php';
+require_once 'databaseClasses/subcategoryClass.php';
 
 $category = new categoryDataAccess();
 $category->readData(1);
