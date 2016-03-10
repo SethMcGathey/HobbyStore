cf2eaff78553f2c73aa4b76542d8f981cea08c07
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-10 13:59:12 -0600

    added reference to parent class file

--- index2.php
+++ index2.php
@@ -2,6 +2,9 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
+require_once 'sessionStart.php'; 
+require_once 'accessDatabaseClass.php'; 
+
 require_once 'sessionStart.php';
 require_once 'databaseClasses/categoryClass.php';
 require_once 'databaseClasses/subcategoryClass.php';
