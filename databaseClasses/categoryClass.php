003a7ecf7a9070845bc8ed9a83e76fca3c7becc1
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-10 14:05:03 -0600

    added parent:: in front of function calls

--- databaseClasses/categoryClass.php
+++ databaseClasses/categoryClass.php
@@ -4,25 +4,25 @@ class categoryDataAccess extends accessDatabase{
     public function readData($selectParam){
         $columns = array($selectParam);
         $sql = "SELECT * FROM category WHERE ?";
-        doSql($sql, $columns);
+        parent::doSql($sql, $columns);
     }
 
     public function createData($name){
         $columns = array($name);
         $sql = "INSERT INTO category (name) values(?)";
-        doSql($sql, $columns);
+        parent::doSql($sql, $columns);
     }
 
 /***/
     public function updateData($name){
         $columns = array($name);
         $sql = "UPDATE category  set name = ? WHERE id = ?";   
-        doSql($sql, $columns);
+        parent::doSql($sql, $columns);
     }
 
     public function deleteData($id){
         $columns = array($id);
         $sql = "DELETE FROM category  WHERE id = ?";
-        doSql($sql, $columns);
+        parent::doSql($sql, $columns);
     }
 }
