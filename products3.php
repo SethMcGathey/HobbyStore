6e13f85007b20dbbcb9d6d947cc3af6afa6cd9ee
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-11 12:28:33 -0600

    added parameter in readData call

--- products3.php
+++ products3.php
@@ -24,7 +24,7 @@ require_once 'databaseClasses/productClass.php';
 					}
 				}else
 				{
-					foreach($product->readData()[1] as $innerRow)
+					foreach($product->readData(1)[1] as $innerRow)
 					{
 						echo '<div class="col-4-lg subcategoryColor' . $num . ' product" id="' . $row['a.id']. '">' . '<img src="data:image/jpeg;base64,' . base64_encode($row['b.image']) . '"width="100px"/> ' . $row['a.name'] . ' ' . $row['a.description'] . ' ' . $row['a.cost'] . ' <a href="addToCart.php?id=' . $row['a.id'] . '">Add to Cart</a></div>';
 					}
