62a8fccb316121d43dd061796908b43966a12817
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-11 15:57:56 -0600

    commented out new code in products3 to check new navigation

--- products3.php
+++ products3.php
@@ -14,7 +14,7 @@ require_once 'databaseClasses/productClass.php';
 			<h1>products.php</h1>
 
 			<?php
-	          	$product = new productDataAccess();
+	          	/*$product = new productDataAccess();
 	          	if(isset($_POST['id']))
 				{
 					foreach($product->readFullProductData(1)[1] as $row)
@@ -28,7 +28,7 @@ require_once 'databaseClasses/productClass.php';
 					{
 						echo '<div class="col-4-lg subcategoryColor' . $num . ' product" id="' . $row['a.id']. '">' . '<img src="data:image/jpeg;base64,' . base64_encode($row['b.image']) . '"width="100px"/> ' . $row['a.name'] . ' ' . $row['a.description'] . ' ' . $row['a.cost'] . ' <a href="addToCart.php?id=' . $row['a.id'] . '">Add to Cart</a></div>';
 					}
-				}
+				}*/
 /*
 				if(isset($_POST['id']))
 				{
