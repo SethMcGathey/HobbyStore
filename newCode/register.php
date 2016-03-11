c1baf32a456987a84786d86066d07d53aa7bea0d
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-11 17:28:39 -0600

    removed extra session start reference

--- newCode/register.php
+++ newCode/register.php
@@ -4,7 +4,6 @@ ini_set('display_errors', 'on');
 require_once 'sessionStart.php'; 
 require_once 'accessDatabaseClass.php'; 
 
-require_once 'sessionStart.php';
 require_once 'databaseClasses/categoryClass.php';
 require_once 'databaseClasses/subcategoryClass.php';
 ?>
