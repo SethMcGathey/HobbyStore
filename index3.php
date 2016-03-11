becd51321d76c801ba4e464f4547f5601b004b4c
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-11 10:35:29 -0600

    changed index3 to match ajax references

--- index3.php
+++ index3.php
@@ -46,22 +46,7 @@ require_once 'databaseClasses/subcategoryClass.php';
           	</div>
 
 			<div id="inner_ajax_Output">
-				<?php
-		          	$subcategory = new subcategoryDataAccess();
-				 
-				  	$num = 0;
-					foreach($subcategory->readDataByCategoryID(1)[$categoryID] as $innerRow)
-					{
-						echo '<a href="products.php?id=' . $innerRow['id']. '"><div class="col-lg-12 subcategoryColor' . $num . '" id="' . $innerRow['id']. '"><p class="leftRight' . $num . '"">' . $innerRow['name'] . '</p></div></a>';
-		                if($num < 1)
-		               	{
-		                	$num++;
-		                }else
-		                {
-							$num = 0;
-		                }
-					}
-				?>
+
 			</div>
 		</div>
 
