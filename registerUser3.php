a236029968ffb9a61563bc73bae733892f123fa7
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-14 15:21:57 -0500

    debugging error

--- registerUser3.php
+++ registerUser3.php
@@ -67,6 +67,7 @@ require_once 'databaseClasses/customerClass.php';
 		{
 			$customer = new customerDataAccess();
 			$customer->createData($firstName, $phoneNumber, $dob, $username, $password, $gender, 1, $email, $lastName);
+			echo "made it";
 			header('Location: login3.php');
 
 
