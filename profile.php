17c798293f2ccec59fa76cacb8fcfb71a6cc48ef
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-15 10:12:40 -0500

    added profile3

--- profile.php
+++ profile.php
@@ -1,3 +1,14 @@
+<?php 
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+require_once 'sessionStart.php'; 
+require_once 'accessDatabaseClass.php'; 
+
+require_once 'sessionStart.php';
+require_once 'databaseClasses/categoryClass.php';
+require_once 'databaseClasses/subcategoryClass.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<?php require_once 'header.php' ?>
---
+++ profile3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<?php require_once 'header.php' ?>
+
+	<body>
+		<?php require_once 'navigation.php' ?> 
+		<div class="container-fluid" id="Not_Ajax_Output">
+		</div>
+
+
+		<div class="container-fluid" id="ajax_Output">
+		</div>
+
+<?php require_once 'footer.php' ?>
+
+	</body>
+
+</html>
