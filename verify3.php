79f826dcc80b7046ca391bf487915bd58c076835
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-15 08:49:24 -0500

    switch incrimintation

--- verify3.php
+++ verify3.php
@@ -24,7 +24,7 @@ require_once 'databaseClasses/customerClass.php';
     $q = $pdo->prepare($sql);
     $q->execute(array($myusername, $mypassword));
     $data = $q->fetch(PDO::FETCH_ASSOC);*/
-    if(isset($data['id'][1]))
+    if(isset($data[2]['id']))
     {
     	
     	$_SESSION['username'] = $data['username'];
