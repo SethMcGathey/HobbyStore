49c21bdf17d4a3941887f95143685cae99cbb866
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-15 11:18:59 -0500

    giving eahc its own row

--- profile3.php
+++ profile3.php
@@ -16,7 +16,7 @@ require_once 'databaseClasses/customer_paymentClass.php';
 		<?php require_once 'navigation.php' ?>
 		<div class="container" id="Not_Ajax_Output">
 			<div class="row">
-				<div class="col-lg-3">
+				<div class="col-lg-6">
 					<h3>Address</h3>
 					<form action="changeAddress.php" method="POST">
 						<p>Street 1:</p><input type="text" placeholder="Street 1" name="street1" id="street1">
@@ -29,25 +29,25 @@ require_once 'databaseClasses/customer_paymentClass.php';
 						<br>
 					</form>
 				</div>
-				<div class="col-lg-3">
+				<div class="col-lg-6">
 					<h3>Current Addresses</h3>
 					<div class="scrollbox">
 					<?php
 						$customer_address = new customer_addressDataAccess();
 						foreach($customer_address->readDataJoinedAddress($_SESSION['customerid'])[1] as $innerRow)
 						{
-							echo 'Street 1: <nobr><p name="street1" id="street1" contenteditable>' . $innerRow['street_one'] . '</p>
-								  Street 2: <nobr><p name="street2" id="street2" contenteditable>' . $innerRow['street_two'] . '</p>
-								  Zipcode: <nobr><p name="zipcode" id="zipcode" contenteditable>' . $innerRow['zipcode'] . '</p>
-								  City: <nobr><p name="city" id="city" contenteditable>' . $innerRow['city'] . '</p>
-								  State: <nobr><p name="state" id="state" contenteditable>' . $innerRow['state'] . '</p>
-								  Country: <nobr><p name="country" id="country" contenteditable>' . $innerRow['country'] . '</p>
+							echo 'Street 1: <p name="street1" id="street1" contenteditable>' . $innerRow['street_one'] . '</p>
+								  Street 2: <p name="street2" id="street2" contenteditable>' . $innerRow['street_two'] . '</p>
+								  Zipcode: <p name="zipcode" id="zipcode" contenteditable>' . $innerRow['zipcode'] . '</p>
+								  City: <p name="city" id="city" contenteditable>' . $innerRow['city'] . '</p>
+								  State: <p name="state" id="state" contenteditable>' . $innerRow['state'] . '</p>
+								  Country: <p name="country" id="country" contenteditable>' . $innerRow['country'] . '</p>
 								  <br>';
 						}
 		            ?>
 		        	</div>
 				</div>
-				<div class="col-lg-3">
+				<div class="col-lg-6">
 					<h3>New Card</h3>
 					<form action="changePayment.php" method="POST">
 						<p>Name on Card:</p><input type="text" placeholder="Name on Card" name="nameOnCard" id="nameOnCard">
@@ -57,17 +57,17 @@ require_once 'databaseClasses/customer_paymentClass.php';
 						<button>Add Card</button>
 					</form>
 				</div>
-				<div class="col-lg-3">
+				<div class="col-lg-6">
 					<h3>Cards on Record</h3>
 					<div class="scrollbox">
 					<?php
 						$customer_address = new customer_paymentDataAccess();
 						foreach($customer_address->readDataJoinedPayments($_SESSION['customerid'])[1] as $innerRow)
 						{
-							echo 'Name on Card: <nobr><p name="nameOnCard" id="nameOnCard" contenteditable>' . $innerRow['card_full_name'] . '</p>
-								  Card Number: <nobr><p name="cardNumber" id="cardNumber" contenteditable>' . $innerRow['card_number'] . '</p>
-								  Security Code: <nobr><p name="securityCode" id="securityCode" contenteditable>' . $innerRow['card_security'] . '</p>
-								  Expires: <nobr><p name="expiration" id="expiration" contenteditable>' . $innerRow['expires_month'] . '/' . $innerRow['expires_year'] . '</p>
+							echo 'Name on Card: <p name="nameOnCard" id="nameOnCard" contenteditable>' . $innerRow['card_full_name'] . '</p>
+								  Card Number: <p name="cardNumber" id="cardNumber" contenteditable>' . $innerRow['card_number'] . '</p>
+								  Security Code: <p name="securityCode" id="securityCode" contenteditable>' . $innerRow['card_security'] . '</p>
+								  Expires: <p name="expiration" id="expiration" contenteditable>' . $innerRow['expires_month'] . '/' . $innerRow['expires_year'] . '</p>
 								  <br>';
 		                }
 		            ?>
