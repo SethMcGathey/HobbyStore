3c30480d873edf602deaaa6b9e77d8e061c28799
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-15 16:33:52 -0500

    added & between url variables

--- js/javascript.js
+++ js/javascript.js
@@ -134,10 +134,10 @@ function changeQuantity(quantity, id, transaction_id){
 $(':button').click(function() {
   var buttonElementId = $(this).attr('id');
   location.href = "updatePayment.php?name=" + $('#nameOnCard' + buttonElementId).text +  
-  "type=" + $('#cardType' + buttonElementId).text + 
-  "number=" + $('#carNumber' + buttonElementId).text +
-  "code=" + $('#securityCode' + buttonElementId).text +
-  "expiration=" + $('#exp' + buttonElementId).text;
+  "&type=" + $('#cardType' + buttonElementId).text + 
+  "&number=" + $('#carNumber' + buttonElementId).text +
+  "&code=" + $('#securityCode' + buttonElementId).text +
+  "&expiration=" + $('#exp' + buttonElementId).text;
 });
 /*$_POST['name'], $_POST['type'], $_POST['number'], $_POST['code'], $_POST['exp'], $_SESSION['customerid']
 nameOnCard,cardType,cardNumber,securityCode,expiration*/
