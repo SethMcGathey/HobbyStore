9944e0c5e2a82fadca80cdd00e0ff36173e2dd76
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-17 17:22:38 -0500

    added get

--- searchProducts.php
+++ searchProducts.php
@@ -6,7 +6,7 @@ require_once 'accessDatabaseClass.php';
 require_once 'databaseClasses/productClass.php';
 
     $product = new productDataAccess();
-    $data = $product->readDataForSearch($_POST["id"]);
+    $data = $product->readDataForSearch($_GET["id"]);
 
 	foreach ($data[1] as $row) {
 	    echo '<div class="col-4-lg subcategoryColor' . $num . ' product" id="' . $row['a.id']. '">' . '<img src="data:image/jpeg;base64,' . base64_encode($row['b.image']) . '"width="100px"/> ' . $row['a.name'] . ' ' . $row['a.description'] . ' ' . $row['a.cost'] . ' <a href="#">Add to Cart</a></div>';
