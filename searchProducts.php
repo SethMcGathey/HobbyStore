d5f4c7e47bcd434571c1f24f3fb3d70cab98ffc6
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-17 17:14:46 -0500

    added access reference and error reporting

--- searchProducts.php
+++ searchProducts.php
@@ -1,6 +1,9 @@
 <?php
-	require_once 'sessionStart.php'; 
-	require_once 'databaseClasses/productClass.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+require_once 'sessionStart.php'; 
+require_once 'accessDatabaseClass.php'; 
+require_once 'databaseClasses/productClass.php';
 
     $product = new productDataAccess();
     $data = $product->readDataForSearch($_POST["id"]);
