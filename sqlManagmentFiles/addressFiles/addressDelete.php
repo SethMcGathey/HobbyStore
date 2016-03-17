6f03378824f72bc671f817ffdc5d54efdf93b81b
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-17 16:57:01 -0500

    added echo

--- sqlManagmentFiles/addressFiles/addressDelete.php
+++ sqlManagmentFiles/addressFiles/addressDelete.php
@@ -16,7 +16,6 @@ require_once '../../databaseClasses/addressClass.php';
     if ( !empty($_POST)) {
         // keep track post values
         $id = $_POST['id'];
-        echo $id;
         $address = new addressDataAccess();
         $address->deleteData($id);
         header("Location: addressIndex.php");
