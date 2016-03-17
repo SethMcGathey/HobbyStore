c4fe60e8368dd046284398e6465ba70a59e1d07e
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-17 11:36:21 -0500

    removed comment out of address

--- accessDatabaseClass.php
+++ accessDatabaseClass.php
@@ -31,6 +31,3 @@ abstract class accessDatabase{
         }
 	}
 }
-
-
-
--- databaseClasses/addressClass.php
+++ databaseClasses/addressClass.php
@@ -37,31 +37,3 @@ class addressDataAccess extends accessDatabase{
 		parent::doSql($sql, $columns);
     }
 }
-
-
-/*
-
-	public function createData($city,$country,$state,$street_one,$street_two,$zipcode,$customer_id){
-		$columns = array($city,$country,$state,$street_one,$street_two,$zipcode);
-
-		doSql($sql, $columns);
-		try{
-		    $pdo = Database::connect();
-	        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	        $sql = "INSERT INTO address (city,country,state,street_one,street_two,zipcode) values(?, ?, ?, ?, ?, ?)";
-	        $q = $pdo->prepare($sql);
-	        $q->execute(array($city,$country,$state,$street_one,$street_two,$zipcode));
-
-	        $address_id = $pdo->lastInsertId();
-	        $sql2 = "INSERT INTO customer_address (customer_id, address_id) values(?, ?)";
-	        $q2 = $pdo->prepare($sql2);
-	        $q2->execute(array($customer_id, $address_id);
-	        Database::disconnect();
-    	}catch(PDOException $error){
-    		header("Location: 500.php");
-    		//header("Location: 500.php?msg=creating%20an%20address");
-    		//echo $error->getMessage();
-    		die();
-    	}
-	}
-*/
