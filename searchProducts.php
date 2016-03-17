5b1f2068eb9344712eafba836fcf71506ae8c621
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-17 17:42:51 -0500

    added  and deleted print completely

--- searchProducts.php
+++ searchProducts.php
@@ -5,9 +5,9 @@ require_once 'sessionStart.php';
 require_once 'accessDatabaseClass.php'; 
 require_once 'databaseClasses/productClass.php';
 
+	$num = 0;
     $product = new productDataAccess();
     $data = $product->readDataForSearch($_GET["id"]);
-    //print_r($data);
 	foreach ($data[1] as $row) {
 	    echo '<div class="col-4-lg subcategoryColor' . $num . ' product" id="' . $row['id']. '">' . '<img src="data:image/jpeg;base64,' . base64_encode($row['image']) . '"width="100px"/> ' . $row['name'] . ' ' . $row['description'] . ' ' . $row['cost'] . ' <a href="#">Add to Cart</a></div>';
 	    if($num < 1){
