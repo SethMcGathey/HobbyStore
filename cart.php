e2122f624d02b23ecb85ff182c4595d3a38cffd3
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-18 11:17:18 -0500

    removed extra '.'

--- cart.php
+++ cart.php
@@ -35,14 +35,16 @@ require_once 'databaseClasses/transactionClass.php';
 				$transaction = new transactionDataAccess();
                 foreach($transaction->readDataForCart($_SESSION['customerid'])[1] as $row)
 				{
-					echo '<div class="row product" id="' . $row['id'] . '">' . 
-			    			'<div class="col-lg-3 cartLine' . $num . '"><img src="data:image/jpeg;base64,' . base64_encode($row['image']) . '"width="100px"/> </div>
+					echo '<div class="row product" id="' . $row['id'] . '"> 
+			    			 <div class="col-lg-3 cartLine' . $num . '"><img src="data:image/jpeg;base64,' . base64_encode($row['image']) . '"width="100px"/> </div>
 			    		  	 <div class="col-lg-3 cartLine' . $num . '">' . $row['name'] . '<br> ' . $row['description'] . '</div> 
 			    		  	 <div class="col-lg-3 cartLine' . $num . '">$' . $row['cost'] . '</div> 
 			    		  	 <div class="col-lg-3 cartLine' . $num . '">
+
 			    		  	 <button href="updateQuantity.php?quantity=' . $row['fullQuantity'] - 1 . '&transactionId=' . $row['transaction_id'] . '&productId=' . $row['id'] . '">-</button>
 			    			 <input type="text" class="textboxWidth" data-arbitraryName=' . $row['id'] . ' value="'. $row['fullQuantity'] . '"> 
 			    			 <button href="updateQuantity.php?quantity=' . $row['fullQuantity'] + 1 . '&transactionId=' . $row['transaction_id'] . '&productId=' . $row['id'] . '">+</button>
+			    			 
 			    			 <div class="rightAlign"><button onclick="changeQuantity('. $row['fullQuantity'] . ', '. $row['id'] . ', '. $row['transaction_id'] . ')">Delete</button></div>
 			    			 <div class="rightAlign"><button href="updateQuantity.php">Update</button></div>
 			    			 <div class="rightAlign"><button onclick="window.location.href=\'removeFromCart.php?productid=' . $row['id'] . '\'">Remove</button></div>
