448bf179a50962d00d5c7582262e9c687f75a1ae
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-18 16:50:12 -0500

    changed setPaymentID to update transaction

--- setPaymentID.php
+++ setPaymentID.php
@@ -1,10 +1,26 @@
 <?php
-	require_once 'sessionStart.php'; 
-	require_once 'database.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+require_once 'sessionStart.php'; 
+
+require_once 'accessDatabaseClass.php';   
+require_once 'databaseClasses/customerClass.php';
+require_once 'databaseClasses/transactionClass.php';
+require_once 'databaseClasses/transaction_addressClass.php';
+
     $pdo = Database::connect();
 
 	$_SESSION['paymentIdForPurchase'] = $_POST['paymentid'];
 
+    $transaction = new transactionDataAccess();
+	$transactionData = $transaction->readCartData($_SESSION['customerid'])[1][0];
+
+
+	$transaction = new transactionDataAccess();
+    $transaction->updateData($transactionData['cart'],$transactionData['timestamp'],$_SESSION['paymentIdForPurchase'],$transactionData['customer_id']);
+
+	
 	header('Location: confirmPurchase.php');
 
 	Database::disconnect();
