dbd85ba54f122b92cfc975999caffe72e0d0b730
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-18 16:30:55 -0500

    added header for styles

--- setPaymentID3.php
+++ setPaymentID3.php
@@ -9,7 +9,7 @@ require_once 'databaseClasses/paymentClass.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
+<?php require_once 'header.php' ?>
 <body>
     <div class="container">
             <div class="row">
