69c64edec54af90e6ffd8ff02db14851ad562f86
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-18 15:44:03 -0500

    removed new variable and added another index

--- setAddress.php
+++ setAddress.php
@@ -12,14 +12,13 @@ require_once 'databaseClasses/transaction_addressClass.php';
 	$_SESSION['addressIdForPurchase'] = $_GET['addressid'];
 	
 	$customer = new customerDataAccess();
-    $data = $customer->readDataById($_SESSION['customerid'])[1];
-    $phone = $data['phone'];
+    $data = $customer->readDataById($_SESSION['customerid'])[1][0];
     print_r($data);
     $transaction = new transactionDataAccess();
 	$transactionData = $transaction->readCartData($_SESSION['customerid'])[1];
 
 	$transaction_address = new transaction_addressDataAccess();
-    $transaction_address->createData($phone, 'payment', $_SESSION['addressIdForPurchase'], 	$transactionData['id']);
+    $transaction_address->createData($data['phone'], 'payment', $_SESSION['addressIdForPurchase'], 	$transactionData['id']);
 
 
 	header('Location: choosePurchasePayment.php');
