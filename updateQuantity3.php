0a0e7c4ede9c35ffa31c7535fbd26a2e37a8ef4d
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-18 14:04:34 -0500

    changed order of updateQuantity code

--- updateQuantity3.php
+++ updateQuantity3.php
@@ -7,45 +7,48 @@ require_once 'databaseClasses/transactionClass.php';
 require_once 'databaseClasses/transaction_productClass.php';
 
 	$_SESSION['transaction_id'] = 0;
+	$quantity = 0;
 
     $transaction = new transactionDataAccess();
 	foreach ($transaction->readCartData($_SESSION['customerid'])[1] as $row) {
 		$_SESSION['transaction_id'] = $row['id'];
 	}
-echo isset($_GET['remove']);
-echo !empty($_GET['remove']);
-if(isset($_GET['remove']) && !empty($_GET['remove'])){
-
-
 	if(trim($_SESSION['transaction_id']) > 0)
 	{
-		$transaction_product = new transaction_productDataAccess();
-		$transaction_product->updateData(0,$_SESSION['transaction_id'],$_GET['id'],$currentId);
 
-	}
-	//header('Location: cart.php');
-
-}else{
-
-
-	if(trim($_SESSION['transaction_id']) > 0)
-	{
-		$quantity = 0;
 		$transaction_product = new transaction_productDataAccess();
 		foreach ($transaction_product->readCartData($_GET['id'],$_SESSION['transaction_id'])[1] as $row) {
 			$currentId = $row['id'];
 			$quantity = $row['quantity'];
-		}
-		if($_GET['direction'] == 'plus')
-		{
-			$transaction_product = new transaction_productDataAccess();
-			$transaction_product->updateData($quantity+1,$_SESSION['transaction_id'],$_GET['id'],$currentId);
-		}else if($_GET['direction'] == 'minus')
-		{
-			$transaction_product = new transaction_productDataAccess();
-			$transaction_product->updateData($quantity-1,$_SESSION['transaction_id'],$_GET['id'],$currentId);
-		}
 
-	}
-	//header('Location: cart.php');
+
+		//echo isset($_GET['remove']);
+		//echo !empty($_GET['remove']);
+		if(isset($_GET['remove']) && !empty($_GET['remove'])){
+
+
+			if(trim($_SESSION['transaction_id']) > 0)
+			{
+				$transaction_product = new transaction_productDataAccess();
+				$transaction_product->updateData(0,$_SESSION['transaction_id'],$_GET['id'],$currentId);
+
+			}
+			//header('Location: cart.php');
+
+		}else{
+
+
+				if($_GET['direction'] == 'plus')
+				{
+					$transaction_product = new transaction_productDataAccess();
+					$transaction_product->updateData($quantity+1,$_SESSION['transaction_id'],$_GET['id'],$currentId);
+				}else if($_GET['direction'] == 'minus')
+				{
+					$transaction_product = new transaction_productDataAccess();
+					$transaction_product->updateData($quantity-1,$_SESSION['transaction_id'],$_GET['id'],$currentId);
+				}
+
+			}
+			//header('Location: cart.php');
+		}
 }
