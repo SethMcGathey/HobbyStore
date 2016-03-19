d39d446c6e24cde6cd90056b174d78982eead88e
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-19 10:24:33 -0500

    removed limit so I can see all products

--- products.php
+++ products.php
@@ -11,13 +11,13 @@ require_once 'databaseClasses/productClass.php';
 	<body>
 		<?php require_once 'navigation.php' ?>
 		<div class="container-fluid" id="Not_Ajax_Output">
-			<h1>products.php</h1>
+			<h1>Products</h1>
 
 			<?php
 				$pdo->setAttribute(PDO::ATTR_FETCH_TABLE_NAMES, true);
 				if(isset($_POST['id']))
 				{
-					$sql = 'SELECT a.id,a.name,a.cost,a.description,b.image FROM product a LEFT JOIN image b ON a.id = b.product_id WHERE a.subcategory_id = ' . $_POST["id"] . ' ORDER BY id LIMIT 5';
+					$sql = 'SELECT a.id,a.name,a.cost,a.description,b.image FROM product a LEFT JOIN image b ON a.id = b.product_id WHERE a.subcategory_id = ' . $_POST["id"] . ' ORDER BY id';
 					//$sql = 'SELECT id,name,cost,description FROM product WHERE subcategory_id = ' . $_POST["id"] . ' ORDER BY id LIMIT 5';
 					foreach ($pdo->query($sql) as $row) {
 					    echo '<div class="col-4-lg subcategoryColor' . $num . ' product" id="' . $row['a.id']. '">' . '<img src="data:image/jpeg;base64,' . base64_encode($row['b.image']) . '"width="100px"/> ' . $row['a.name'] . ' ' . $row['a.description'] . ' ' . $row['a.cost'] . ' <a href="addToCart.php?id=' . $row['a.id'] . '">Add to Cart</a></div>';
@@ -48,3 +48,32 @@ require_once 'databaseClasses/productClass.php';
 
 	<?php require_once 'footer.php' ?>
 </html>
+
+
+
+<!--
+Strategy Games
+Family Games
+Abstract Games
+Classic Games
+Baby Toys -> CLassic Toys
+Sports -> Superhero
+Princess -> Grown Up
+Cowboy -> All Ages
+
+
+
+clue 2
+aggravation 3
+go 4
+backgammon 5
+the duke
+good housekeeping
+ransburger puzzle
+time square puzzle
+african
+world
+magic 8
+super
+captain
+-->
