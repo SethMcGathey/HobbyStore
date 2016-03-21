b9bdb64b607a8e2cc935ba4eb01f714a73aae48c
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-21 00:10:27 -0500

    added hobby store reference

--- sqlManagmentFiles/addressFiles/addressIndex.php
+++ sqlManagmentFiles/addressFiles/addressIndex.php
@@ -11,10 +11,10 @@ require_once '../../databaseClasses/addressClass.php';
 
 <!DOCTYPE html>
 <html lang="en">
-<?php require_once $_SERVER['DOCUMENT_ROOT'].'/header.php' ?>
+<?php require_once $_SERVER['DOCUMENT_ROOT'].'/HobbyStore/header.php' ?>
 
 <body>
-    <?php require $_SERVER['DOCUMENT_ROOT'].'/navigation.php' ?>
+    <?php require $_SERVER['DOCUMENT_ROOT'].'/HobbyStore/navigation.php' ?>
     <div class="container">
 
             <div class="row">
