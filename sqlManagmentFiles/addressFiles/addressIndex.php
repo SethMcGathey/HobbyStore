bd5a0fa1653f5ec1c5785024db627466d747acd8
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-21 00:08:45 -0500

    added DocumentRoot reference

--- sqlManagmentFiles/addressFiles/addressIndex.php
+++ sqlManagmentFiles/addressFiles/addressIndex.php
@@ -11,10 +11,10 @@ require_once '../../databaseClasses/addressClass.php';
 
 <!DOCTYPE html>
 <html lang="en">
-<?php require_once '../../header.php' ?>
+<?php require_once $_SERVER['DOCUMENT_ROOT'].'/header.php' ?>
 
 <body>
-    <?php require '../../navigation.php' ?>
+    <?php require $_SERVER['DOCUMENT_ROOT'].'/navigation.php' ?>
     <div class="container">
 
             <div class="row">
--- sqlManagmentFiles/addressFiles/addressUpdate.php
+++ sqlManagmentFiles/addressFiles/addressUpdate.php
@@ -86,10 +86,10 @@ require_once '../../databaseClasses/addressClass.php';
 
 <!DOCTYPE html>
 <html lang="en">
-<?php require_once '../../header.php' ?>
+<?php require_once $_SERVER['DOCUMENT_ROOT'].'/header.php' ?>
 
 <body>
-    <?php require '../../navigation.php' ?>
+    <?php require $_SERVER['DOCUMENT_ROOT'].'/navigation.php' ?>
     <div class="container">
 
 
