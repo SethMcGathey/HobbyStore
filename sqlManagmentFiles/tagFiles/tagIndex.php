e9518b586202e7d30c71f5121a3385fc092645f7
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-20 20:51:22 -0500

    added error reporting

--- sqlManagmentFiles/tagFiles/tagIndex.php
+++ sqlManagmentFiles/tagFiles/tagIndex.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
 require_once '../../sessionStart.php'; 
 
 require_once '../../accessDatabaseClass.php'; 
@@ -6,12 +8,12 @@ require_once '../../databaseClasses/tagClass.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<?php require_once 'header.php' ?>
+  <?php require_once 'header.php' ?>
 
-<body>
-    <?php require_once 'navigation.php' ?>
+  <body>
+      <?php require_once 'navigation.php' ?>
 
-    <div class="container">
+      <div class="container">
             <div class="row">
                 <h3>PHP CRUD Grid</h3>
             </div>
