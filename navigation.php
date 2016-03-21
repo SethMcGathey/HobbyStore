50bd46a679c064ec1caa80d1bea48d6a7fd7a7c9
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-21 12:58:46 -0500

    added sr-only line of navbar

--- navigation.php
+++ navigation.php
@@ -9,13 +9,14 @@ require_once 'databaseClasses/subcategoryClass.php';
   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
       <a class="navbar-brand" href="index.php">Clash Games</a>
     </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
+    <div class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
         <?php
           $ifActive = array("/index.php"=>"", "/products.php"=>"", "/sqlManagmentFiles/productFiles/productIndex.php"=>"", "/contact.php"=>"", "/cart.php"=>"", "/logout.php"=>"", "/profile.php"=>"", "/register.php"=>"", "/login.php"=>"");
