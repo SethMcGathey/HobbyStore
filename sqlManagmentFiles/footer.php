3b9b31a3b8076f4a27d417bd931aed66be4dcacd
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-27 21:05:20 -0500

    added margin at bottom to ensure all content is visiable

--- sqlManagmentFiles/footer.php
+++ sqlManagmentFiles/footer.php
@@ -1,6 +1,5 @@
 <div class="panel-footer footer">
-	<h5>Clash Games LLC</h5>		
-	<h5>Seth McGathey</h5>		
+	<h5>Clash Games LLC</h5>			
 	<h5>708-297-0996</h5>		
 	<h5>officialclashgames@gmail.com</h5>
 	<h5><a href="siteMap.php">Site Map</a></h5>
