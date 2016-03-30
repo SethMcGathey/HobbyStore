f8b5951a14cd1b5e2ac850182d8ddebf3d908438
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-30 08:37:28 -0500

    added new layout to single product page

--- singleProductPage.php
+++ singleProductPage.php
@@ -23,7 +23,33 @@ require_once 'databaseClasses/productClass.php';
           $data = $product->readProductForSinglePageData($_GET['id'])[1];
           $num = 0;
           foreach ($data as $row) {
-              echo '<div class="col-4-lg subcategoryColor' . $num . ' product" id="' . $row['id']. '">' . '<img alt="' . $row['description'] . '" title="' . $row['description'] . '" src="data:image/jpeg;base64,' . base64_encode($row['image']) . '"width="100px"/> ' . $row['name'] . ' ' . $row['description'] . ' ' . $row['cost'] . ' <a href="addToCart.php?id=' . $row['id'] . '">Add to Cart</a></div>';
+              //echo '<div class="col-4-lg subcategoryColor' . $num . ' product" id="' . $row['id']. '">' . '<img alt="' . $row['description'] . '" title="' . $row['description'] . '" src="data:image/jpeg;base64,' . base64_encode($row['image']) . '"width="100px"/> ' . $row['name'] . ' ' . $row['description'] . ' ' . $row['cost'] . ' <a href="addToCart.php?id=' . $row['id'] . '">Add to Cart</a></div>';
+              
+
+              echo '<div class="row subcategoryColor product" id="' . $row['id']. '">
+                              <div class="col-lg-6" >
+                                  <a href="singleProductPage.php?id='.$row['id'].'"><img class="productsImage" alt="' . $row['description'] . '" title="' . $row['description'] . '"  src="data:image/jpeg;base64,' . base64_encode($row['image']) . '"width="100px"/></a>
+                              </div>
+                              <div class="col-lg-6">
+                                  <div class="row">
+                                      <div class="col-lg-12 name">
+                                          <span class="nameSpan">' . $row['name'] . '</span>
+                                      </div>
+                                      <div class="col-lg-6 discription" >
+                                          <span class="discriptionSpan">' . $row['description'] . '</span>
+                                      </div> 
+                                      <div class="col-lg-6 price">
+                                          <span class="priceSpan">$' . $row['cost'] . '.00</span>
+                                      </div>
+                                      <div class="col-lg-6 addToCart">
+                                          <span class="addToCartSpan"><a href="addToCart.php?id=' . $row['id'] . '">Add to Cart</a></span>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>';
+
+
+
               if($num < 1){
                 $num++;
               }else
