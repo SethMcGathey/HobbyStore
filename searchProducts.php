dc3dd6b3b42381d32ee87912d9a952d4bdb84a8f
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-30 08:24:58 -0500

    removed alias from search

--- searchProducts.php
+++ searchProducts.php
@@ -14,21 +14,21 @@ require_once 'databaseClasses/productClass.php';
 
         echo '<div class="row subcategoryColor product" id="' . $row['a.id']. '">
                 <div class="col-lg-6" >
-                    <a href="singleProductPage.php?id='.$row['a.id'].'"><img class="productsImage" alt="' . $row['b.description'] . '" title="' . $row['b.description'] . '"  src="data:image/jpeg;base64,' . base64_encode($row['b.image']) . '"width="100px"/></a>
+                    <a href="singleProductPage.php?id='.$row['id'].'"><img class="productsImage" alt="' . $row['description'] . '" title="' . $row['description'] . '"  src="data:image/jpeg;base64,' . base64_encode($row['image']) . '"width="100px"/></a>
                 </div>
                 <div class="col-lg-6">
                     <div class="row">
                         <div class="col-lg-12 name">
-                            <span class="nameSpan">' . $row['a.name'] . '</span>
+                            <span class="nameSpan">' . $row['name'] . '</span>
                         </div>
                         <div class="col-lg-6 discription" >
-                            <span class="discriptionSpan">' . $row['a.description'] . '</span>
+                            <span class="discriptionSpan">' . $row['description'] . '</span>
                         </div> 
                         <div class="col-lg-6 price">
-                            <span class="priceSpan">$' . $row['a.cost'] . '.00</span>
+                            <span class="priceSpan">$' . $row['cost'] . '.00</span>
                         </div>
                         <div class="col-lg-6 addToCart">
-                            <span class="addToCartSpan"><a href="addToCart.php?id=' . $row['a.id'] . '">Add to Cart</a></span>
+                            <span class="addToCartSpan"><a href="addToCart.php?id=' . $row['id'] . '">Add to Cart</a></span>
                         </div>
                     </div>
                 </div>
