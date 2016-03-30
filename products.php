3bf0d674f000ea9aa5d52fa9740c00e2f3a54d00
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-29 19:18:36 -0500

    added new layout into product page

--- products.php
+++ products.php
@@ -22,6 +22,36 @@ require_once 'databaseClasses/productClass.php';
 					foreach ($pdo->query($sql) as $row) {
 					    echo '<div class="col-4-lg subcategoryColor' . $num . ' product" id="' . $row['a.id']. '">' . '<a href="singleProductPage.php?id='.$row['a.id'].'"><img class="productsImage" alt="' . $row['b.description'] . '" title="' . $row['b.description'] . '"  src="data:image/jpeg;base64,' . base64_encode($row['b.image']) . '"width="100px"/></a> <p>' . $row['a.name'] . '</p> <p>' . $row['a.description'] . '</p> <p>$' . $row['a.cost'] . '.00</p> <a href="addToCart.php?id=' . $row['a.id'] . '">Add to Cart</a></div>';
 					
+
+
+
+					    echo '<div class="row subcategoryColor' . $num . ' product" id="' . $row['a.id']. '">
+								<div class="col-lg-6" >
+									<a href="singleProductPage.php?id='.$row['a.id'].'"><img class="productsImage" alt="' . $row['b.description'] . '" title="' . $row['b.description'] . '"  src="data:image/jpeg;base64,' . base64_encode($row['b.image']) . '"width="100px"/></a>
+								</div>
+								<div class="col-lg-6">
+									<div class="row">
+										<div class="col-lg-12 name">
+											<span class="nameSpan">' . $row['a.name'] . '</span>
+										</div>
+										<div class="col-lg-6 discription" >
+											<span class="discriptionSpan">' . $row['a.description'] . '</span>
+										</div> 
+										<div class="col-lg-6 price">
+											<span class="priceSpan">$' . $row['a.cost'] . '.00</span>
+										</div>
+										<div class="col-lg-6 addToCart">
+											<span class="addToCartSpan"><a href="addToCart.php?id=' . $row['a.id'] . '">Add to Cart</a></span>
+										</div>
+									</div>
+								</div>
+							</div>'
+
+
+
+
+
+
 						if($num < 1){
 				    		$num++;
 				    	}else
